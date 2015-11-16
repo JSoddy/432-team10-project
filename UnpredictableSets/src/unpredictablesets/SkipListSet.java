8d700f73ef580fd4d0ab4394347024070ae92fa2
James Soddy <jsoddy@gmail.com>
2015-11-16 08:00:49 -0700

    Implementing skip-list

--- UnpredictableSets/src/unpredictablesets/SkipListSet.java
+++ UnpredictableSets/src/unpredictablesets/SkipListSet.java
@@ -8,7 +8,7 @@ package unpredictablesets;
 public class SkipListSet {
   
   // We will need some kind of head element instance variable
-  private element head;
+  private Element head;
   // We will need a size instance variable
   private int size;
   
@@ -18,7 +18,7 @@ public class SkipListSet {
   }
   
   // We need a private method to search through the skip list
-  private element find(){
+  private Element find(){
     
     return null;
   }
@@ -46,22 +46,41 @@ public class SkipListSet {
     return size;
   }
   
-  // We will need a private class for our elements
-  private class element {
+  // !!! After some thought, we should probably change this to a double-linked
+  //  list in both the horizontal and vertical directions
+  // We will need a private class for our data elements
+  private class Element {
     // Will need a reference to the next element on this level
+    private Element next;
     // Will need a reference to the lower level
+    private Element down;
     // Will need some data
-    private int data; // Maybe this doesn't want to be an int. We can change it.
+    private final int data; // Maybe this doesn't want to be an int. Dunno.
+    
+    private Element(int inData){
+      data = inData;
+    }
     
     // Will need to be able to return its next element
+    private Element getNext(){
+      return next;
+    }
     
     // Will need to be able to change its next element
+    private void setNext(Element newNext){
+      next = newNext;
+    }
     
     // Will need to be able to return its down element
-    
+    private Element getDown(){ // Get down, get down! Get down, get down!
+      return down;
+    }
     // I don't think there is ever a reason to change its down element...
     
     // Will need to be able to return its data
+    private int getData(){
+      return data;
+    }
   }
   
 }
