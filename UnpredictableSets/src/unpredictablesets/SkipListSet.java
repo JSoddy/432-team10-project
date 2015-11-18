bf7bf7c1ad06fc0616c0fac8d356ecd4d6ffd521
James Soddy <jsoddy@gmail.com>
2015-11-17 22:44:36 -0700

    Implementing addElement, and putting diagnostic code in main();

--- UnpredictableSets/src/unpredictablesets/SkipListSet.java
+++ UnpredictableSets/src/unpredictablesets/SkipListSet.java
@@ -1,4 +1,3 @@
-
 package unpredictablesets;
 
 /**
@@ -6,9 +5,10 @@ package unpredictablesets;
  * @author Group 10
  */
 public class SkipListSet {
+
   private static final int POS_INFTY = Integer.MAX_VALUE;
   private static final int NEG_INFTY = Integer.MIN_VALUE;
-  
+
   // We will need some kind of head element instance variable
   private Element head;
   // I guess a tail element wouldn't hurt.
@@ -17,48 +17,63 @@ public class SkipListSet {
   private int size;
   // Should we store the height?
   private int maxHeight;
-  
+
   // We will need a constructor
-  public SkipListSet(){
+  public SkipListSet() {
+    // initialize size and height
+    size = 0;
+    maxHeight = 4;
     // Probably we should create dummy head and tail elements?
     head = createElement(maxHeight, NEG_INFTY);
     tail = createElement(maxHeight, POS_INFTY);
-    link(head, tail);
-    // initialize size and height
-    size = 0;
-    maxHeight = 2;
+    initialize();
   }
-  
+
+  private void initialize() {
+    Element headLevel = head;
+    Element tailLevel = tail;
+    while (headLevel != null) {
+      headLevel.next = tailLevel;
+      tailLevel.previous = headLevel;
+      headLevel = headLevel.down;
+      tailLevel = tailLevel.down;
+    }
+  }
+
   // Private method to link two elements together.
   //  Takes as arguments two elements, links them together as long as there is
   //  not another element between them
-  private void link(Element first, Element second){
+  private void link(Element first, Element second) {
     // We should make sure we have both elements at the same level
-    while(first.getHeight() > second.getHeight()){
+    while (first.getHeight() > second.getHeight()) {
       first = first.getDown();
     }
-    while(second.getHeight() > first.getHeight()){
+    while (second.getHeight() > first.getHeight()) {
       second = second.getDown();
     }
-    
+
     // Now link in our new element until we've gotten something else in the way
     Element third = first.getNext();
-    while (third != null && third.getData() > second.getData()){
+    while (third != null && third.getData() > second.getData()) {
       first.setNext(second);
       second.setNext(third);
       third.setPrevious(second);
       second.setPrevious(first);
       first = first.getDown();
       second = second.getDown();
-      third = first.getNext();
+      if (first != null) {
+        third = first.getNext();
+      } else {
+        third = null;
+      }
     }
   }
-  
+
   // Private method to extract an element and repair the links around it
-  private void unLink(Element toRemove){
+  private void unLink(Element toRemove) {
     Element prev;
     Element next;
-    while(toRemove != null){
+    while (toRemove != null) {
       prev = toRemove.getPrevious();
       next = toRemove.getNext();
       prev.setNext(next);
@@ -66,11 +81,11 @@ public class SkipListSet {
       toRemove = toRemove.getDown();
     }
   }
-  
+
   // Private method to create an element for insertion into our list
   //  Takes as parameters the height and data of the new element
   //  Returns the top level of the new element
-  private Element createElement(int elementHeight, int inData){
+  private Element createElement(int elementHeight, int inData) {
     // "down" from the bottom layer should be null
     Element newElement = null;
     // Every element has height at least 1
@@ -78,90 +93,129 @@ public class SkipListSet {
     //  dereferencing null pointer inside the loop
     newElement = new Element(inData, 0, newElement);
     // Loop through, adding elements as necessary to reach elementHeight
-    for (int i = 1; i < elementHeight; i++){
+    for (int i = 1; i < elementHeight; i++) {
       newElement = new Element(inData, i, newElement);
       newElement.down.setUp(newElement); // the lower element needs to point up
     }
     return newElement;
   }
-  
-  // !!! NYI
+
   // We need a private method to search through the skip list
-  private Element find(int toFind){
-      Element currentElement = head;
-      while(currentElement!=null){
-      if(currentElement.data == toFind){
-          return currentElement;
+  private Element find(int toFind) {
+    Element currentElement = head;
+    while (currentElement != null) {
+      if (currentElement.data == toFind) {
+        return currentElement;
+      } else if (currentElement.next.data > toFind) {
+        if (currentElement.height == 0) {
+          return null;
+        } else {
+          currentElement = currentElement.down;
+        }
+      } else {
+        currentElement = currentElement.next;
       }
-      else if(currentElement.next.data>toFind){
-          if(currentElement.height == 0){
-              return null;
-          }
-          else{
-              currentElement = currentElement.down;}
-              }
-      else {currentElement = currentElement.next;}
-      }
-    return null; // !!! placeholder
+    }
+    return null;
   }
-  
+
+  public void diag() {
+    System.out.println("HEAD:");
+    Element current = head;
+    Element next;
+    while (current != null) {
+      next = current.next;
+      System.out.println("Current: " + current.data);
+      if (next != null) {
+        System.out.println("Next: " + next.data);
+      } else {
+        System.out.println("Next = null");
+      }
+      current = current.down;
+    }
+    System.out.println("TAIL:");
+    current = tail;
+    Element previous;
+    while (current != null) {
+      previous = current.previous;
+      System.out.println("Current: " + current.data);
+      if (previous != null) {
+        System.out.println("Previous: " + previous.data);
+      } else {
+        System.out.println("Previous = null");
+      }
+      current = current.down;
+    }
+  }
+
   // We will need a membership operation method
-  public boolean isInSet(int toFind){
-    return  find(toFind) != null; // If find returns null, it's not there
+  public boolean isInSet(int toFind) {
+    return find(toFind) != null; // If find returns null, it's not there
   }
-  
+
   // !!! NYI
   // We will need an add operation method
-  public boolean addElement(int toAdd){
-    if (isInSet(toAdd)){
+  public boolean addElement(int toAdd) {
+    if (isInSet(toAdd)) {
       return false;
     } else {
       Element newElement = createElement(chooseHeight(), toAdd);
       Element current = head;
-      
-      
-    }
-    
-    return false; // !!! placeholder
-  }
-  
-  // We will need a remove operation method
-  public boolean removeElement(int toRemove){
-    Element found = find(toRemove);
-    if (found == null){
-      return false;
-    } else {
-      unLink(found);
+
+      while (current != null) {
+        if (current.next.data > toAdd) {
+          link(current, newElement);
+          current = current.down;
+        } else if (current.next.data == toAdd) {
+          current = current.down;
+        } else {
+          current = current.next;
+        }
+      }
+      size++;
       return true;
     }
   }
-  
+
+  // We will need a remove operation method
+  public boolean removeElement(int toRemove) {
+    Element found = find(toRemove);
+    if (found == null) {
+      return false;
+    } else {
+      unLink(found);
+      size--;
+      return true;
+    }
+  }
+
   // We need some size returning method
-  public int size(){
+  public int size() {
     return size;
   }
-  
+
   // !!! NYI
   // We will need a method that will return the contents of the set, and
   //  do height re-adjustments in the process
   //  What should this return? An array list? !!!
-  public void getContents(){
-    
+  public void getContents() {
+
   }
-  
+
   // Method to randomly choose a height for a newly inserted node
-  private int chooseHeight(){
-    boolean heads = Math.random() < .50;
+  private int chooseHeight() {
+    boolean heads = (Math.random() < 0.50);
     int height = 0;
-    while (heads && height < maxHeight){
+    while (heads && height < maxHeight) {
       height++;
-      heads = Math.random() < .50;
+      heads = (Math.random() < .50);
     }
     return height;
   }
-  
+
   // We will need a private class for our data elements
   private class Element {
+
     // Will need a reference to the next element on this level
     private Element next;
     // And to the preceeding element
@@ -174,64 +228,64 @@ public class SkipListSet {
     private final int height;
     // Will need some data
     private final int data; // Maybe this doesn't want to be an int. Dunno.
-    
+
     // Constructor. I think data and the 'down' element are enough.
-    private Element(int inData, int inHeight, Element inDown){
+    private Element(int inData, int inHeight, Element inDown) {
       data = inData;
       down = inDown;
       height = inHeight;
     }
-    
+
     // Will need to be able to return its next element
-    private Element getNext(){
+    private Element getNext() {
       return next;
     }
-    
+
     // Will need to be able to change its next element
-    private void setNext(Element newNext){
+    private void setNext(Element newNext) {
       next = newNext;
     }
-    
+
     // Will need to be able to return its previous element
-    private Element getPrevious(){
+    private Element getPrevious() {
       return previous;
     }
-    
+
     // Will need to be able to change its previous element
-    private void setPrevious(Element newPrevious){
+    private void setPrevious(Element newPrevious) {
       previous = newPrevious;
     }
-    
+
     // Will need to be able to return its down element
-    private Element getDown(){ // Get down, get down! Get down, get down!
+    private Element getDown() { // Get down, get down! Get down, get down!
       return down;
     }
-    
+
     // I don't think there is ever a reason to change its down element...
     // But, shoot. No harm in doing it anyway
-    private void setDown(Element newDown){
+    private void setDown(Element newDown) {
       down = newDown;
     }
-    
+
     // Will need to be able to return its up element
-    private Element getUp(){ // You slept through your alarm!
+    private Element getUp() { // You slept through your alarm!
       return up;
     }
-    
+
     // I guess we might as well have this method as well
-    private void setUp(Element newUp){ // It's a trap!
+    private void setUp(Element newUp) { // It's a trap!
       up = newUp;
     }
-    
+
     // I suppose someone might want the height
-    private int getHeight(){
+    private int getHeight() {
       return height;
     }
-    
+
     // Will need to be able to return its data
-    private int getData(){
+    private int getData() {
       return data;
     }
   }
-  
+
 }
--- UnpredictableSets/src/unpredictablesets/UnpredictableSets.java
+++ UnpredictableSets/src/unpredictablesets/UnpredictableSets.java
@@ -11,8 +11,27 @@ public class UnpredictableSets {
    */
   public static void main(String[] args) {
     
+    SkipListSet testSet = new SkipListSet();
     
+    testSet.diag();
     
+    testSet.addElement(5);
+    System.out.println(testSet.addElement(3));
+    testSet.addElement(999);
+    testSet.addElement(77);
+    
+    testSet.diag();
+    
+    testSet.removeElement(77);
+    
+    System.out.println(testSet.isInSet(800));
+    System.out.println(testSet.isInSet(3));
+    System.out.println(testSet.isInSet(77));
+    
+    testSet.removeElement(3);
+    System.out.println(testSet.isInSet(3));
+    
+    testSet.diag();
   }
   
 }
